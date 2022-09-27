dc13d7df4b4f055754f200e2b81bc9a6c2e12bb8
CNFeffery <fefferypzy@gmail.com>
2022-09-27 23:02:24 +0800

    build: 初始化0.0.1-rc8版本

--- demos/FundFlowTest/app.py
+++ demos/FundFlowTest/app.py
@@ -40,7 +40,83 @@ demo_data = dict(
                       'value': {'text': '100,000 Yuan', 'subText': '2019-08-03'}},
                      {'source': '1',
                       'target': '9',
-                      'value': {'text': '100,000 Yuan', 'subText': '2019-08-03'}}]}
+                      'value': {'text': '100,000 Yuan', 'subText': '2019-08-03'}}]},
+    demo2={
+        'nodes': [
+            {
+                'id': '任务A',
+                'value': {
+                    'text': '任务A',
+                    'status': 'success',
+                    'start_datetime': '2022-09-27 10:00:00',
+                    'end_datetime': '2022-09-27 10:02:00'
+                }
+            },
+            {
+                'id': '任务B',
+                'value': {
+                    'text': '任务B',
+                    'status': 'processing',
+                    'start_datetime': '2022-09-27 10:02:02'
+                }
+            },
+            {
+                'id': '任务C',
+                'value': {
+                    'text': '任务C',
+                    'status': 'error',
+                    'start_datetime': '2022-09-27 10:02:05'
+                }
+            },
+            {
+                'id': '任务D',
+                'value': {
+                    'text': '任务D',
+                    'status': 'default'
+                }
+            },
+            {
+                'id': '任务E',
+                'value': {
+                    'text': '任务E',
+                    'status': 'default'
+                }
+            },
+            {
+                'id': '任务F',
+                'value': {
+                    'text': '任务F',
+                    'status': 'default'
+                }
+            }
+        ],
+        'edges': [
+            {
+                'source': '任务A',
+                'target': '任务B'
+            },
+            {
+                'source': '任务A',
+                'target': '任务C'
+            },
+            {
+                'source': '任务B',
+                'target': '任务D'
+            },
+            {
+                'source': '任务C',
+                'target': '任务E'
+            },
+            {
+                'source': '任务D',
+                'target': '任务F'
+            },
+            {
+                'source': '任务E',
+                'target': '任务F'
+            }
+        ]
+    }
 )
 
 
@@ -75,6 +151,51 @@ app.layout = html.Div(
     };
 }
 '''
+                }
+            ),
+            style={
+                'height': '500px',
+                'border': '1px solid #dbdbda'
+            }
+        ),
+        html.H3('资金流向图-自由流向'),
+        html.Div(
+            fact.AntdFundFlow(
+                data=demo_data.get('demo2'),
+                behaviors=['drag-canvas', 'zoom-canvas'],
+                nodeCfg={
+                    'style': {
+                        'func': '''
+(e) => {
+    if (e.value.status === "success") {
+        return {
+            stroke: 'green'
+        }
+    } else if (e.value.status === "error") {
+        return {
+            stroke: "red"
+        }
+    } else if (e.value.status === "processing") {
+        return {
+            stroke: "#40a9ff"
+        }
+    } else if (e.value.status === "default") {
+        return {
+            stroke: "#c8c6c4"
+        }
+    } 
+}'''
+                    },
+                    'nodeStateStyles': False
+                },
+                edgeCfg={
+                    'style': {
+                        'stroke': '#c8c6c4'
+                    },
+                    'endArrow': {
+                        'fill': '#c8c6c4'
+                    },
+                    'edgeStateStyles': False
                 }
             ),
             style={
--- src/FefferyAntdCharts.jl
+++ src/FefferyAntdCharts.jl
@@ -3,7 +3,7 @@ module FefferyAntdCharts
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1-rc7"
+const version = "0.0.1-rc8"
 
 include("jl/''_antdarea.jl")
 include("jl/''_antdbar.jl")
@@ -33,14 +33,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "feffery_antd_charts.min.js",
-    external_url = "https://unpkg.com/feffery_antd_charts@0.0.1-rc7/feffery_antd_charts/feffery_antd_charts.min.js",
+    external_url = "https://unpkg.com/feffery_antd_charts@0.0.1-rc8/feffery_antd_charts/feffery_antd_charts.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "feffery_antd_charts.min.js.map",
-    external_url = "https://unpkg.com/feffery_antd_charts@0.0.1-rc7/feffery_antd_charts/feffery_antd_charts.min.js.map",
+    external_url = "https://unpkg.com/feffery_antd_charts@0.0.1-rc8/feffery_antd_charts/feffery_antd_charts.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
