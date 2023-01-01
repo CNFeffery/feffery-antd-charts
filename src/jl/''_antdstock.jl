9e4105d0e5baccbfaf09538d29154a216b379f21
CNFeffery <fefferypzy@gmail.com>
2023-01-01 22:21:22 +0800

    feat: AntdStock新增基础事件监听功能

--- src/jl/''_antdstock.jl
+++ src/jl/''_antdstock.jl
@@ -29,6 +29,18 @@ Those elements have the following types:
 - `locale` (a value equal to: 'zh-CN', 'en-US'; optional)
 - `meta` (optional)
 - `padding` (Real | Array of Reals | a value equal to: 'auto'; optional)
+- `recentlyAreaClickRecord` (optional): . recentlyAreaClickRecord has the following type: lists containing elements 'timestamp', 'data'.
+Those elements have the following types:
+  - `timestamp` (Real; optional)
+  - `data` (Bool | Real | String | Dict | Array; optional)
+- `recentlyLegendInfo` (optional): . recentlyLegendInfo has the following type: lists containing elements 'triggerItemName', 'items'.
+Those elements have the following types:
+  - `triggerItemName` (Bool | Real | String | Dict | Array; optional)
+  - `items` (Array of Dicts; optional)
+- `recentlyTooltipChangeRecord` (optional): . recentlyTooltipChangeRecord has the following type: lists containing elements 'timestamp', 'data'.
+Those elements have the following types:
+  - `timestamp` (Real; optional)
+  - `data` (Array of Dicts; optional)
 - `renderer` (a value equal to: 'canvas', 'svg'; optional)
 - `risingFill` (String; optional)
 - `slider` (optional)
@@ -45,7 +57,7 @@ Those elements have the following types:
 - `yField` (Array of Strings; required)
 """
 function ''_antdstock(; kwargs...)
-        available_props = Symbol[:id, :annotations, :appendPadding, :autoFit, :className, :data, :downloadTrigger, :fallingFill, :height, :key, :label, :legend, :limitInPlot, :loading_state, :locale, :meta, :padding, :renderer, :risingFill, :slider, :stockStyle, :style, :theme, :tooltip, :width, :xAxis, :xField, :yAxis, :yField]
+        available_props = Symbol[:id, :annotations, :appendPadding, :autoFit, :className, :data, :downloadTrigger, :fallingFill, :height, :key, :label, :legend, :limitInPlot, :loading_state, :locale, :meta, :padding, :recentlyAreaClickRecord, :recentlyLegendInfo, :recentlyTooltipChangeRecord, :renderer, :risingFill, :slider, :stockStyle, :style, :theme, :tooltip, :width, :xAxis, :xField, :yAxis, :yField]
         wild_props = Symbol[]
         return Component("''_antdstock", "AntdStock", "feffery_antd_charts", available_props, wild_props; kwargs...)
 end
