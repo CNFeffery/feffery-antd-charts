5bc48e3186a5ce73f815f918010db4f343ed2524
CNFeffery <fefferypzy@gmail.com>
2023-01-25 22:13:59 +0800

    chore: 更新dash最低依赖版本至2.8.0

--- setup.py
+++ setup.py
@@ -16,8 +16,10 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
-    install_requires=[],
-    classifiers = [
+    install_requires=[
+        'dash>=2.8.0'
+    ],
+    classifiers=[
         'Framework :: Dash',
-    ],    
+    ],
 )
