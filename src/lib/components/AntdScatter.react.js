1e2108bd6f9d31e173a205eef2769224ac79f328
CNFeffery <fefferypzy@gmail.com>
2024-01-21 21:36:54 +0800

    chore: 调整AntdScatter参数

--- src/lib/components/AntdScatter.react.js
+++ src/lib/components/AntdScatter.react.js
@@ -199,10 +199,10 @@ AntdScatter.propTypes = {
     ]),
 
     // 设置图表渲染方式为'canvas'或'svg'模式，默认为'canvas'
-    renderer: PropTypes.string,
+    renderer: PropTypes.oneOf(['canvas', 'svg']),
 
     // 设置语言，可选的有'zh-CN'与'en-US'
-    locale: PropTypes.string,
+    locale: PropTypes.oneOf(['zh-CN', 'en-US']),
 
     // 设置是否对超出绘图区域的几何元素进行裁剪
     limitInPlot: PropTypes.bool,
@@ -287,6 +287,7 @@ AntdScatter.propTypes = {
 
 // 设置默认参数
 AntdScatter.defaultProps = {
+    locale: 'zh-CN',
     downloadTrigger: 'download-trigger'
 }
 
