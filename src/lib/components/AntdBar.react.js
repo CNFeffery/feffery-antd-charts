f4b4a6ee7fd5dcb69b52f7887088aaeb83348502
CNFeffery <fefferypzy@gmail.com>
2024-01-21 21:20:14 +0800

    chore: 调整AntdBar参数

--- src/lib/components/AntdBar.react.js
+++ src/lib/components/AntdBar.react.js
@@ -206,10 +206,10 @@ AntdBar.propTypes = {
     ]),
 
     // 设置图表渲染方式为'canvas'或'svg'模式，默认为'canvas'
-    renderer: PropTypes.string,
+    renderer: PropTypes.oneOf(['canvas', 'svg']),
 
     // 设置语言，可选的有'zh-CN'与'en-US'
-    locale: PropTypes.string,
+    locale: PropTypes.oneOf(['zh-CN', 'en-US']),
 
     // 设置是否对超出绘图区域的几何元素进行裁剪
     limitInPlot: PropTypes.bool,
@@ -294,6 +294,7 @@ AntdBar.propTypes = {
 
 // 设置默认参数
 AntdBar.defaultProps = {
+    locale: 'zh-CN',
     downloadTrigger: 'download-trigger'
 }
 
