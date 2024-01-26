3ffce72144c13c3ed75de4dced0a4b6a075ea631
CNFeffery <fefferypzy@gmail.com>
2024-01-26 16:13:35 +0800

    refactor: 重构AntdRose中meta参数处理逻辑 #9

--- src/lib/fragments/plots/AntdRose.react.js
+++ src/lib/fragments/plots/AntdRose.react.js
@@ -115,10 +115,10 @@ export default class AntdRose extends Component {
         // 预处理元信息
         if (meta) {
             config.meta = cloneDeep(meta);
-            for (let i in Object.keys(meta)) {
+            for (let metaKey of Object.keys(meta)) {
                 // 若meta中当前字段属性下的formatter具有自定义函数func属性
-                if (meta[Object.keys(meta)[i]]?.formatter?.func) {
-                    config.meta[Object.keys(meta)[i]].formatter = eval(meta[Object.keys(meta)[i]].formatter.func)
+                if (meta[metaKey]?.formatter?.func) {
+                    config.meta[metaKey].formatter = eval(meta[metaKey].formatter.func)
                 }
             }
         }
