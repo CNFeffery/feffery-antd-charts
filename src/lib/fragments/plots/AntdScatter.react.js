6965e376bfca615a8e1d620be9b8d2fc832db5d5
CNFeffery <fefferypzy@gmail.com>
2024-01-28 22:07:04 +0800

    feat: AntdScatter新增主题融合功能 #12

--- src/lib/fragments/plots/AntdScatter.react.js
+++ src/lib/fragments/plots/AntdScatter.react.js
@@ -6,7 +6,7 @@
 import { Scatter } from '@ant-design/plots';
 import React, { Component } from 'react';
 import { isUndefined, omitBy, intersection, cloneDeep } from 'lodash';
-import { difference } from '../../components/utils';
+import { difference, withTheme } from '../../components/utils';
 import { propTypes, defaultProps } from '../../components/AntdScatter.react';
 
 // 定义不触发重绘的参数数组
@@ -142,7 +142,12 @@ export default class AntdScatter extends Component {
             height,
             autoFit,
             renderer,
-            theme,
+            theme: (
+                // 融合内置主题与自定义主题
+                theme && theme.withTheme ?
+                    withTheme(theme.withTheme, theme) :
+                    theme
+            ),
             interactions,
             locale,
             limitInPlot
