43aefd92dee5a462049d810b486e56234c5dbb0c
CNFeffery <fefferypzy@gmail.com>
2024-01-28 22:08:02 +0800

    feat: AntdStock新增主题融合功能 #12

--- src/lib/fragments/plots/AntdStock.react.js
+++ src/lib/fragments/plots/AntdStock.react.js
@@ -7,7 +7,7 @@
 import { Stock } from '@ant-design/plots';
 import React, { Component } from 'react';
 import { isUndefined, omitBy, intersection, cloneDeep } from 'lodash';
-import { difference } from '../../components/utils';
+import { difference, withTheme } from '../../components/utils';
 import { propTypes, defaultProps } from '../../components/AntdStock.react';
 
 // 定义不触发重绘的参数数组
@@ -132,7 +132,12 @@ export default class AntdStock extends Component {
             height,
             autoFit,
             renderer,
-            theme,
+            theme: (
+                // 融合内置主题与自定义主题
+                theme && theme.withTheme ?
+                    withTheme(theme.withTheme, theme) :
+                    theme
+            ),
             interactions,
             locale,
             limitInPlot
