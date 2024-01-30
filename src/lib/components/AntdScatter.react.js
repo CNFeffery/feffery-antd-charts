be8607b619a1a1927e0aab046006c39cc87b2d88
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:27:24 +0800

    feat: AntdScatter完善interactions参数 #11

--- src/lib/components/AntdScatter.react.js
+++ src/lib/components/AntdScatter.react.js
@@ -18,7 +18,8 @@ import {
     baseStyle,
     textBaseStyle,
     themeBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdScatter = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdScatter.react'));
@@ -258,8 +259,10 @@ AntdScatter.propTypes = {
     // 主题配置
     theme: themeBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
