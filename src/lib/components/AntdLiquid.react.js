98d9412c57c552fc5ee131cf70ac350287028a42
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:24:00 +0800

    feat: AntdLiquid完善interactions参数 #11

--- src/lib/components/AntdLiquid.react.js
+++ src/lib/components/AntdLiquid.react.js
@@ -10,7 +10,8 @@ import {
     areaBaseStyle,
     themeBasePropTypes,
     patternBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdLiquid = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdLiquid.react'));
@@ -218,8 +219,10 @@ AntdLiquid.propTypes = {
      */
     pattern: patternBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
