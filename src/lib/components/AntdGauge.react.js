38f07e084a70032f26b61e30f980d72c1427c6c8
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:22:22 +0800

    feat: AntdGauge完善interactions参数 #11

--- src/lib/components/AntdGauge.react.js
+++ src/lib/components/AntdGauge.react.js
@@ -12,7 +12,8 @@ import {
     textBaseStyle,
     axisBasePropTypes,
     themeBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdGauge = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdGauge.react'));
@@ -220,8 +221,10 @@ AntdGauge.propTypes = {
     // 主题配置
     theme: themeBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
