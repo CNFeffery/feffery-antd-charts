fdbd1f24e8b141a843f55618ddebd67c6004b90c
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:25:21 +0800

    feat: AntdRadar完善interactions参数 #11

--- src/lib/components/AntdRadar.react.js
+++ src/lib/components/AntdRadar.react.js
@@ -17,7 +17,8 @@ import {
     tooltipBasePropTypes,
     annotationsBasePropTypes,
     themeBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdRadar = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdRadar.react'));
@@ -259,8 +260,10 @@ AntdRadar.propTypes = {
     // 主题配置
     theme: themeBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
