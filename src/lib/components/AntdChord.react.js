a6175470433f821239f45cc3771e7f43e51e38db
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:20:34 +0800

    feat: AntdChord完善interactions参数 #11

--- src/lib/components/AntdChord.react.js
+++ src/lib/components/AntdChord.react.js
@@ -11,7 +11,8 @@ import {
     labelBasePropTypes,
     areaBaseStyle,
     themeBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdChord = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdChord.react'));
@@ -140,8 +141,10 @@ AntdChord.propTypes = {
     // 主题配置
     theme: themeBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
