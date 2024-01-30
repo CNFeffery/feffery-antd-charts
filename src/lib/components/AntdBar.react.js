250fa6c07ac2448f43f487c8222a793451ca7cdd
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:11:30 +0800

    feat: AntdBar完善interactions参数 #11

--- src/lib/components/AntdBar.react.js
+++ src/lib/components/AntdBar.react.js
@@ -19,7 +19,8 @@ import {
     textBaseStyle,
     themeBasePropTypes,
     patternBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdBar = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdBar.react'));
@@ -271,8 +272,10 @@ AntdBar.propTypes = {
      */
     pattern: patternBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
