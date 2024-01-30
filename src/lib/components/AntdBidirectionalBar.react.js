225503d0370d2cd202237f0f384d988ed19d68fa
CNFeffery <fefferypzy@gmail.com>
2024-01-30 22:18:21 +0800

    feat: AntdBidirectionalBar完善interactions参数 #11

--- src/lib/components/AntdBidirectionalBar.react.js
+++ src/lib/components/AntdBidirectionalBar.react.js
@@ -15,7 +15,8 @@ import {
     annotationsBasePropTypes,
     baseStyle,
     themeBasePropTypes,
-    animationBasePropTypes
+    animationBasePropTypes,
+    interactionsBasePropTypes
 } from './BasePropTypes.react';
 
 const LazyAntdBidirectionalBar = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdBidirectionalBar.react'));
@@ -155,8 +156,10 @@ AntdBidirectionalBar.propTypes = {
     // 主题配置
     theme: themeBasePropTypes,
 
-    // 交互事件配置
-    interactions: PropTypes.array,
+    /**
+     * 交互功能项配置
+     */
+    interactions: interactionsBasePropTypes,
 
     loading_state: PropTypes.shape({
         /**
