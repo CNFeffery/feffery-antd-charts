0d7e616a0cd8cbc58d71b8a3697906a27d4d3cf2
CNFeffery <fefferypzy@gmail.com>
2024-02-04 11:20:16 +0800

    test: 补充AntdHeatmap常用功能测试用例 #5

--- usage.py
+++ usage.py
@@ -22,10 +22,121 @@ app.layout = html.Div(
                     'type': 'cat',
                 },
             }
+        ),
+        fact.AntdHeatmap(
+            data=(
+                requests
+                .get('https://gw.alipayobjects.com/os/bmw-prod/68d3f380-089e-4683-ab9e-4493200198f9.json')
+                .json()
+            ),
+            xField='name',
+            yField='country',
+            colorField='value',
+            shape='circle',
+            sizeRatio=0.5,
+            color=['#0d5fbb', '#7eadfc', '#fd8b6f', '#aa3523'],
+            label={
+                'style': {
+                    'fill': '#fff',
+                    'shadowBlur': 2,
+                    'shadowColor': 'rgba(0, 0, 0, .45)',
+                },
+            }
+        ),
+        fact.AntdHeatmap(
+            data=(
+                requests
+                .get('https://gw.alipayobjects.com/os/bmw-prod/68d3f380-089e-4683-ab9e-4493200198f9.json')
+                .json()
+            ),
+            xField='name',
+            yField='country',
+            colorField='value',
+            sizeField='value',
+            shape='square',
+            color=['#dddddd', '#9ec8e0', '#5fa4cd', '#2e7ab6', '#114d90'],
+            label={
+                'style': {
+                    'fill': '#fff',
+                    'shadowBlur': 2,
+                    'shadowColor': 'rgba(0, 0, 0, .45)',
+                },
+            }
+        ),
+        fact.AntdHeatmap(
+            height=800,
+            data=(
+                requests
+                .get('https://gw.alipayobjects.com/os/antvdemo/assets/data/polar-heatmap.json')
+                .json()
+            ),
+            xField='time',
+            yField='week',
+            colorField='value',
+            legend=True,
+            color='#BAE7FF-#1890FF-#1028ff',
+            coordinate={
+                'type': 'polar',
+                'cfg': {
+                    'innerRadius': 0.2,
+                },
+            },
+            heatmapStyle={
+                'stroke': '#f5f5f5',
+                'opacity': 0.8,
+            },
+            meta={
+                'time': {
+                    'type': 'cat',
+                },
+                'value': {
+                    'min': 0,
+                    'max': 1,
+                },
+            },
+            xAxis={
+                'line': None,
+                'grid': None,
+                'tickLine': None,
+                'label': {
+                    'offset': 12,
+                    'style': {
+                        'fill': '#666',
+                        'fontSize': 12,
+                        'textBaseline': 'top',
+                    },
+                },
+            },
+            yAxis={
+                'top': True,
+                'line': None,
+                'grid': None,
+                'tickLine': None,
+                'label': {
+                    'offset': 0,
+                    'style': {
+                        'fill': '#fff',
+                        'textAlign': 'center',
+                        'shadowBlur': 2,
+                        'shadowColor': 'rgba(0, 0, 0, .45)',
+                    },
+                },
+            },
+            tooltip={
+                'showMarkers': False,
+            },
+            interactions=[
+                {
+                    'type': 'element-active',
+                }
+            ]
         )
     ],
     style={
-        'padding': 50
+        'padding': '50px 100px',
+        'display': 'flex',
+        'flexDirection': 'column',
+        'gap': 100
     }
 )
 
