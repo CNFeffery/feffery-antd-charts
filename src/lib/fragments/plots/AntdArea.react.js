257d3f9f76d2a18d4e34b84df76babd7c2287584
CNFeffery <fefferypzy@gmail.com>
2024-03-15 15:07:45 +0800

    feat: AntdArea新增参数pixelRatio #16

--- src/lib/fragments/plots/AntdArea.react.js
+++ src/lib/fragments/plots/AntdArea.react.js
@@ -95,6 +95,7 @@ export default class AntdArea extends Component {
             padding,
             appendPadding,
             renderer,
+            pixelRatio,
             locale,
             limitInPlot,
             legend,
@@ -142,6 +143,7 @@ export default class AntdArea extends Component {
             height,
             autoFit,
             renderer,
+            pixelRatio,
             theme: (
                 // 融合内置主题与自定义主题
                 theme && theme.withTheme ?
--- usage.py
+++ usage.py
@@ -1,4 +1,5 @@
 import dash
+import random
 from dash import html
 import feffery_antd_charts as fact
 
@@ -6,213 +7,22 @@ app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        html.H2('基础韦恩图'),
-        fact.AntdVenn(
+        fact.AntdArea(
             data=[
                 {
-                    'sets': ['A'],
-                    'size': 12,
-                    'label': 'A',
-                },
-                {
-                    'sets': ['B'],
-                    'size': 12,
-                    'label': 'B',
-                },
-                {
-                    'sets': ['C'],
-                    'size': 12,
-                    'label': 'C',
-                },
-                {
-                    'sets': ['A', 'B'],
-                    'size': 2,
-                    'label': 'A&B',
-                },
-                {
-                    'sets': ['A', 'C'],
-                    'size': 2,
-                    'label': 'A&C',
-                },
-                {
-                    'sets': ['B', 'C'],
-                    'size': 2,
-                    'label': 'B&C',
-                },
-                {
-                    'sets': ['A', 'B', 'C'],
-                    'size': 1,
-                },
-            ],
-            setsField='sets',
-            sizeField='size',
-            pointStyle={
-                'fillOpacity': 0.85,
-            },
-            legend={
-                'position': 'top'
-            }
-        ),
-        html.H2('设置颜色叠加模式'),
-        fact.AntdVenn(
-            data=[
-                {
-                    'sets': ['A'],
-                    'size': 12,
-                    'label': 'A',
-                },
-                {
-                    'sets': ['B'],
-                    'size': 12,
-                    'label': 'B',
-                },
-                {
-                    'sets': ['C'],
-                    'size': 12,
-                    'label': 'C',
-                },
-                {
-                    'sets': ['A', 'B'],
-                    'size': 2,
-                    'label': 'A&B',
-                },
-                {
-                    'sets': ['A', 'C'],
-                    'size': 2,
-                    'label': 'A&C',
-                },
-                {
-                    'sets': ['B', 'C'],
-                    'size': 2,
-                    'label': 'B&C',
-                },
-                {
-                    'sets': ['A', 'B', 'C'],
-                    'size': 1,
-                },
-            ],
-            setsField='sets',
-            sizeField='size',
-            blendMode='overlay',
-            pointStyle={
-                'fillOpacity': 0.85,
-            }
-        ),
-        html.H2('格式化tooltip'),
-        fact.AntdVenn(
-            data=[
-                {
-                    'sets': ['A'],
-                    'size': 12,
-                    'label': 'A',
-                },
-                {
-                    'sets': ['B'],
-                    'size': 12,
-                    'label': 'B',
-                },
-                {
-                    'sets': ['C'],
-                    'size': 12,
-                    'label': 'C',
-                },
-                {
-                    'sets': ['A', 'B'],
-                    'size': 2,
-                    'label': 'A&B',
-                },
-                {
-                    'sets': ['A', 'C'],
-                    'size': 2,
-                    'label': 'A&C',
-                },
-                {
-                    'sets': ['B', 'C'],
-                    'size': 2,
-                    'label': 'B&C',
-                },
-                {
-                    'sets': ['A', 'B', 'C'],
-                    'size': 1,
-                    'label': 'A&B&C',
-                },
-            ],
-            setsField='sets',
-            sizeField='size',
-            pointStyle={
-                'fillOpacity': 0.85,
-            },
-            tooltip={
-                'fields': ['label', 'size'],
-                'formatter': {
-                    'func': '''(datum) => {
-        return {
-          name: datum.label,
-          value: datum.size,
-        };
-      }'''
+                    'x': i,
+                    'y': random.random()
                 }
-            }
-        ),
-        html.H2('设置label'),
-        fact.AntdVenn(
-            data=[
-                {
-                    'sets': ['A'],
-                    'size': 12,
-                    'label': 'A',
-                },
-                {
-                    'sets': ['B'],
-                    'size': 12,
-                    'label': 'B',
-                },
-                {
-                    'sets': ['C'],
-                    'size': 12,
-                    'label': 'C',
-                },
-                {
-                    'sets': ['A', 'B'],
-                    'size': 2,
-                    'label': 'A&B',
-                },
-                {
-                    'sets': ['A', 'C'],
-                    'size': 2,
-                    'label': 'A&C',
-                },
-                {
-                    'sets': ['B', 'C'],
-                    'size': 2,
-                    'label': 'B&C',
-                },
-                {
-                    'sets': ['A', 'B', 'C'],
-                    'size': 1,
-                    'label': 'A&B&C',
-                },
+                for i in range(100)
             ],
-            setsField='sets',
-            sizeField='size',
-            pointStyle={
-                'fillOpacity': 0.85,
-            },
-            label={
-                'offsetY': 7,
-                'style': {
-                    'fontSize': 14,
-                },
-                'formatter': {
-                    'func': '''(datum) => `${datum.sets.join('&')}: ${datum.size}`'''
-                },
-            }
+            pixelRatio=3,
+            xField='x',
+            yField='y',
+            smooth=True
         )
     ],
     style={
-        'padding': '50px 100px',
-        'display': 'flex',
-        'flexDirection': 'column'
+        'padding': '50px 100px'
     }
 )
 
