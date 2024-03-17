8d45321a28b9b0fa0af4ff2d3d63c04829a09dfe
CNFeffery <fefferypzy@gmail.com>
2024-03-17 18:16:07 +0800

    feat: AntdColumn新增参数pixelRatio #16

--- src/lib/fragments/plots/AntdColumn.react.js
+++ src/lib/fragments/plots/AntdColumn.react.js
@@ -104,6 +104,7 @@ export default class AntdColumn extends Component {
             padding,
             appendPadding,
             renderer,
+            pixelRatio,
             locale,
             limitInPlot,
             legend,
@@ -161,6 +162,7 @@ export default class AntdColumn extends Component {
             height,
             autoFit,
             renderer,
+            pixelRatio,
             theme: (
                 // 融合内置主题与自定义主题
                 theme && theme.withTheme ?
