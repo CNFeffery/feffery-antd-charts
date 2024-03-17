7b5211e187c442e40df6fcce1065915efbc28100
CNFeffery <fefferypzy@gmail.com>
2024-03-17 18:48:12 +0800

    feat: AntdVenn新增参数pixelRatio #16

--- src/lib/fragments/plots/AntdVenn.react.js
+++ src/lib/fragments/plots/AntdVenn.react.js
@@ -87,6 +87,7 @@ export default class AntdVenn extends Component {
             padding,
             appendPadding,
             renderer,
+            pixelRatio,
             locale,
             limitInPlot,
             legend,
@@ -127,6 +128,7 @@ export default class AntdVenn extends Component {
             height,
             autoFit,
             renderer,
+            pixelRatio,
             theme: (
                 // 融合内置主题与自定义主题
                 theme && theme.withTheme ?
