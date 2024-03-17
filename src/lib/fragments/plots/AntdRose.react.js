4e09939b0cc479efc9214f022f170147ebbb6744
CNFeffery <fefferypzy@gmail.com>
2024-03-17 18:36:19 +0800

    feat: AntdRose新增参数pixelRatio #16

--- src/lib/fragments/plots/AntdRose.react.js
+++ src/lib/fragments/plots/AntdRose.react.js
@@ -93,6 +93,7 @@ export default class AntdRose extends Component {
             padding,
             appendPadding,
             renderer,
+            pixelRatio,
             locale,
             limitInPlot,
             xAxis,
@@ -143,6 +144,7 @@ export default class AntdRose extends Component {
             height,
             autoFit,
             renderer,
+            pixelRatio,
             theme: (
                 // 融合内置主题与自定义主题
                 theme && theme.withTheme ?
