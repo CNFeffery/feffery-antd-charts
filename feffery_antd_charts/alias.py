4d7db5887ed191f20956f01f918bf49515686e95
CNFeffery <fefferypzy@gmail.com>
2024-05-05 11:28:54 +0800

    chore: 更新alias别名模块

--- feffery_antd_charts/alias.py
+++ feffery_antd_charts/alias.py
@@ -8,46 +8,66 @@ from .AntdColumn import AntdColumn as Column
 from .AntdDualAxes import AntdDualAxes as DualAxes
 from .AntdFunnel import AntdFunnel as Funnel
 from .AntdGauge import AntdGauge as Gauge
+from .AntdHeatmap import AntdHeatmap as Heatmap
 from .AntdHistogram import AntdHistogram as Histogram
 from .AntdLine import AntdLine as Line
 from .AntdLiquid import AntdLiquid as Liquid
 from .AntdPie import AntdPie as Pie
+from .AntdProgress import AntdProgress as Progress
 from .AntdRadar import AntdRadar as Radar
+from .AntdRadialBar import AntdRadialBar as RadialBar
+from .AntdRingProgress import AntdRingProgress as RingProgress
 from .AntdRose import AntdRose as Rose
 from .AntdSankey import AntdSankey as Sankey
 from .AntdScatter import AntdScatter as Scatter
 from .AntdStock import AntdStock as Stock
 from .AntdSunburst import AntdSunburst as Sunburst
+from .AntdTinyArea import AntdTinyArea as TinyArea
+from .AntdTinyColumn import AntdTinyColumn as TinyColumn
 from .AntdTinyLine import AntdTinyLine as TinyLine
 from .AntdTreemap import AntdTreemap as Treemap
+from .AntdVenn import AntdVenn as Venn
+from .AntdViolin import AntdViolin as Violin
+from .AntdWaterfall import AntdWaterfall as Waterfall
 from .AntdWordCloud import AntdWordCloud as WordCloud
 from .AntdDecompositionTree import AntdDecompositionTree as DecompositionTree
 from .AntdFundFlow import AntdFundFlow as FundFlow
+from .AntdRadialTree import AntdRadialTree as RadialTree
 
 __all__ = [
-    "Area",
-    "Bar",
-    "BidirectionalBar",
-    "Box",
-    "Bullet",
-    "Chord",
-    "Column",
-    "DualAxes",
-    "Funnel",
-    "Gauge",
-    "Histogram",
-    "Line",
-    "Liquid",
-    "Pie",
-    "Radar",
-    "Rose",
-    "Sankey",
-    "Scatter",
-    "Stock",
-    "Sunburst",
-    "TinyLine",
-    "Treemap",
-    "WordCloud",
-    "DecompositionTree",
-    "FundFlow"
+    'Area',
+    'Bar',
+    'BidirectionalBar',
+    'Box',
+    'Bullet',
+    'Chord',
+    'Column',
+    'DualAxes',
+    'Funnel',
+    'Gauge',
+    'Heatmap',
+    'Histogram',
+    'Line',
+    'Liquid',
+    'Pie',
+    'Progress',
+    'Radar',
+    'RadialBar',
+    'RingProgress',
+    'Rose',
+    'Sankey',
+    'Scatter',
+    'Stock',
+    'Sunburst',
+    'TinyArea',
+    'TinyColumn',
+    'TinyLine',
+    'Treemap',
+    'Venn',
+    'Violin',
+    'Waterfall',
+    'WordCloud',
+    'DecompositionTree',
+    'FundFlow',
+    'RadialTree',
 ]
