9c926ba10dbc0eb80b352251530d2781317dee29
CNFeffery <fefferypzy@gmail.com>
2024-07-18 21:57:02 +0800

    tests: AntdPie交互类型pie-statistic-active测试用例

--- usage.py
+++ usage.py
@@ -1,196 +1,38 @@
 import dash
-import json
 from dash import html
 import feffery_antd_charts as fact
-from dash.dependencies import Input, Output
+from feffery_dash_utils.style_utils import style
 
-app = dash.Dash(__name__, compress=True)
-
-demo_data = {
-    'id': 'Modeling Methods',
-    'children': [
-        {
-            'id': 'Classification',
-            'children': [
-                {
-                    'id': 'Logistic regression',
-                    'value': 'Logistic regression',
-                },
-                {
-                    'id': 'Linear discriminant analysis',
-                    'value': 'Linear discriminant analysis',
-                },
-                {'id': 'Rules', 'value': 'Rules'},
-                {
-                    'id': 'Decision trees',
-                    'value': 'Decision trees',
-                },
-                {
-                    'id': 'Naive Bayes',
-                    'value': 'Naive Bayes',
-                },
-                {
-                    'id': 'K nearest neighbor',
-                    'value': 'K nearest neighbor',
-                },
-                {
-                    'id': 'Probabilistic neural network',
-                    'value': 'Probabilistic neural network',
-                },
-                {
-                    'id': 'Support vector machine',
-                    'value': 'Support vector machine',
-                },
-            ],
-            'value': 'Classification',
-        },
-        {
-            'id': 'Consensus',
-            'children': [
-                {
-                    'id': 'Models diversity',
-                    'children': [
-                        {
-                            'id': 'Different initializations',
-                            'value': 'Different initializations',
-                        },
-                        {
-                            'id': 'Different parameter choices',
-                            'value': 'Different parameter choices',
-                        },
-                        {
-                            'id': 'Different architectures',
-                            'value': 'Different architectures',
-                        },
-                        {
-                            'id': 'Different modeling methods',
-                            'value': 'Different modeling methods',
-                        },
-                        {
-                            'id': 'Different training sets',
-                            'value': 'Different training sets',
-                        },
-                        {
-                            'id': 'Different feature sets',
-                            'value': 'Different feature sets',
-                        },
-                    ],
-                    'value': 'Models diversity',
-                },
-                {
-                    'id': 'Methods',
-                    'children': [
-                        {
-                            'id': 'Classifier selection',
-                            'value': 'Classifier selection',
-                        },
-                        {
-                            'id': 'Classifier fusion',
-                            'value': 'Classifier fusion',
-                        },
-                    ],
-                    'value': 'Methods',
-                },
-                {
-                    'id': 'Common',
-                    'children': [
-                        {
-                            'id': 'Bagging',
-                            'value': 'Bagging',
-                        },
-                        {
-                            'id': 'Boosting',
-                            'value': 'Boosting',
-                        },
-                        {
-                            'id': 'AdaBoost',
-                            'value': 'AdaBoost',
-                        },
-                    ],
-                    'value': 'Common',
-                },
-            ],
-            'value': 'Consensus',
-        },
-        {
-            'id': 'Regression',
-            'children': [
-                {
-                    'id': 'Multiple linear regression',
-                    'value': 'Multiple linear regression',
-                },
-                {
-                    'id': 'Partial least squares',
-                    'value': 'Partial least squares',
-                },
-                {
-                    'id': 'Multi-layer feedforward neural network',
-                    'value': 'Multi-layer feedforward neural network',
-                },
-                {
-                    'id': 'General regression neural network',
-                    'value': 'General regression neural network',
-                },
-                {
-                    'id': 'Support vector regression',
-                    'value': 'Support vector regression',
-                },
-            ],
-            'value': 'Regression',
-        },
-    ],
-    'value': 'Modeling Methods',
-}
+app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.Div(
-            fact.AntdRadialTree(
-                id='demo-chart',
-                data=demo_data,
-                nodeCfg={
-                    'type': 'diamond',
+        fact.AntdPie(
+            data=[
+                {'type': '分类一', 'value': 27},
+                {'type': '分类二', 'value': 25},
+                {'type': '分类三', 'value': 18},
+                {'type': '分类四', 'value': 15},
+                {'type': '分类五', 'value': 10},
+                {'type': '其他', 'value': 5},
+            ],
+            angleField='value',
+            colorField='type',
+            innerRadius=0.6,
+            label={'type': 'inner'},
+            meta={
+                'value': {
+                    'formatter': {
+                        'func': '(v) => `${v} ¥`'
+                    },
                 },
-                behaviors=[
-                    'drag-canvas',
-                    'zoom-canvas',
-                    'drag-node',
-                    'click-select',
-                ],
-                style={'height': '100%'},
-            ),
-            style={'height': 700},
-        ),
-        html.Pre(id='output'),
+            },
+            statistic={'title': {'content': '总金额'}},
+            interactions=[{'type': 'pie-statistic-active'}],
+        )
     ],
-    style={'padding': 50},
+    style=style(padding=50),
 )
-
-
-@app.callback(
-    Output('output', 'children'),
-    Input('demo-chart', 'recentlyNodeClickRecord'),
-    Input('demo-chart', 'recentlyNodeDoubleClickRecord'),
-    Input('demo-chart', 'recentlyEdgeClickRecord'),
-    Input('demo-chart', 'recentlyEdgeDoubleClickRecord'),
-)
-def update_output(
-    recentlyNodeClickRecord,
-    recentlyNodeDoubleClickRecord,
-    recentlyEdgeClickRecord,
-    recentlyEdgeDoubleClickRecord,
-):
-    return json.dumps(
-        dict(
-            recentlyNodeClickRecord=recentlyNodeClickRecord,
-            recentlyNodeDoubleClickRecord=recentlyNodeDoubleClickRecord,
-            recentlyEdgeClickRecord=recentlyEdgeClickRecord,
-            recentlyEdgeDoubleClickRecord=recentlyEdgeDoubleClickRecord,
-        ),
-        indent=4,
-        ensure_ascii=False,
-    )
-
 
 if __name__ == '__main__':
     app.run(debug=True)
