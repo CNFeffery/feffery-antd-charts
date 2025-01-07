c8205deea08bb81bd8ae9cfc7081bfe5b6b63ada
CNFeffery <fefferypzy@gmail.com>
2025-01-07 20:22:04 +0800

    chore: 重构AntdLine参数注释

--- src/lib/components/AntdLine.react.js
+++ src/lib/components/AntdLine.react.js
@@ -23,6 +23,9 @@ import {
 
 const LazyAntdLine = React.lazy(() => import(/* webpackChunkName: "plots" */ '../fragments/plots/AntdLine.react'));
 
+/**
+ * 折线图组件AntdLine
+ */
 const AntdLine = (props) => {
     return (
         <Suspense fallback={null}>
@@ -31,219 +34,304 @@ const AntdLine = (props) => {
     );
 }
 
-// 定义参数或属性
 AntdLine.propTypes = {
-    // 部件id
+    /**
+     * 组件唯一id
+     */
     id: PropTypes.string,
 
-    // 辅助强制刷新
+    /**
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
     key: PropTypes.string,
 
-    // css类名
+    /**
+     * 当前组件css类名
+     */
     className: PropTypes.string,
 
-    // 自定义css字典
+    /**
+     * 当前组件css样式
+     */
     style: PropTypes.object,
 
-    // 定义绘图所需数据，必须参数
+    /**
+     * 必填，定义绘图所需数据
+     */
     data: PropTypes.arrayOf(PropTypes.object).isRequired,
 
-    // 定义字段预处理元信息
+    /**
+     * 以字段为单位，配置图表数据元信息，来定义所涉及数据的类型和展示方式
+     */
     meta: metaBasePropTypes,
 
-    // 定义作为x轴的字段名
+    /**
+     * 必填，图表x轴字段
+     */
     xField: PropTypes.string.isRequired,
 
-    // 定义作为y轴的字段名
+    /**
+     * 必填，图表y轴字段
+     */
     yField: PropTypes.string.isRequired,
 
-    // 定义作为分组依据的字段名
+    /**
+     * 图表分组字段
+     */
     seriesField: PropTypes.string,
 
-    // 设置是否以平滑曲线方式渲染折线，默认为false
+    /**
+     * 是否渲染为平滑曲线
+     * 默认值：`false`
+     */
     smooth: PropTypes.bool,
 
-    // 对应阶梯折线图类型的阶梯曲折方式，可选的有'hv'、'vh'、'hvh'及'vhv'
-    // 其中'h'表示horizontal，'v'表示vertical，譬如`vh`就代表先竖直方向再水平方向
+    /**
+     * 阶梯折线图类型，可选项有`'hv'`、`'vh'`、`'hvh'`、`'vhv'`，其中`'h'`代表水平方向，`'v'`代表竖直方向，譬如`'vh`就代表先竖直再水平
+     */
     stepType: PropTypes.string,
 
-    // 设置针对折线图中缺失值的绘制策略，true表示连接，false表示断开，默认为true
+    /**
+     * 针对折线图中的缺失值片段，是否对空数据段两端进行连线
+     * 默认值：`true`
+     */
     connectNulls: PropTypes.bool,
 
-    // 在存在seriesField分组字段时，用于设置是否将折线堆叠起来，默认为false
+    /**
+     * 当`seriesField`有效时，是否进行分组折线堆叠
+     * 默认值：`false`
+     */
     isStack: PropTypes.bool,
 
-    // 用于手动设置调色方案，接受css中合法的所有颜色值，当传入单个字符串时，所有折线沿用此颜色值
-    // 当传入数组时，会视作调色盘方案对seriesField区分的不同系列进行着色
-    // 当传入对象时，会解析出其'func'属性对应的字符串，解析为函数，以支持更为自由的seriesField->色彩映射
+    /**
+     * 控制折线颜色，具体见在线文档相关说明
+     */
     color: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.arrayOf(PropTypes.string),
         PropTypes.exact({
-            // 传入字符串形式的js函数体源码，例如
-            // (ref) => {
-            //     if (ref.series === '系列一'){
-            //         return 'red'
-            //     }
-            //     return 'blue'
-            // }
+            /**
+             * js函数体字符串
+             */
             func: PropTypes.string
         })
     ]),
 
-    // 用于设置折线样式，常规方式下接受对象用于设置全局折线样式
-    // 亦可传入字符串对应的js函数体，实现针对不同seriesField返回不同样式，例如
-    // (ref) => {
-    //     if (ref.seriesField === 'a'){
-    //         return {
-    //             lineDash: [4, 4],
-    //             opacity: 1,
-    //           };
-    //     }
-    //     return { opacity: 0.5 };
-    // }
+    /**
+     * 控制折线样式，具体见在线文档相关说明
+     */
     lineStyle: PropTypes.oneOfType([
         baseStyle,
         PropTypes.exact({
-            // 回调模式
+            /**
+             * js函数体字符串
+             */
             func: PropTypes.string
         })
     ]),
 
-    // 用于设置折线图折点的样式
+    /**
+     * 配置折点相关参数，具体见在线文档相关说明
+     */
     point: PropTypes.exact({
-        // 设置折点颜色，支持单字符串、字符串数组以及对象传入func定义js函数体，函数格式同lineStyle
+        /**
+         * 配置折点颜色，具体见在线文档相关说明
+         */
         color: PropTypes.oneOfType([
             PropTypes.string,
             PropTypes.arrayOf(PropTypes.string),
             PropTypes.exact({
+                /**
+                 * js函数体字符串
+                 */
                 func: PropTypes.string
             })
         ]),
 
-        // 设置折点形状，支持单字符串或对象传入func定义js函数体，函数格式同lineStyle
-        // 单字符时可选的样式有'circle'、'square'、'line'、'diamond'、'triangle'、'triangle-down'、'hexagon'、
-        // 'bowtie'、'cross'、'tick'、'plus'及'hyphen'
+        /**
+         * 配置折点形状，具体见在线文档相关说明
+         */
         shape: PropTypes.oneOfType([
             PropTypes.string,
             PropTypes.exact({
+                /**
+                 * js函数体字符串
+                 */
                 func: PropTypes.string
             })
         ]),
 
-        // 设置折点通用style属性，支持对象传入，当对象中具有func属性时，会视作func回调模式处理
+        /**
+         * 配置折点样式，具体见在线文档相关说明
+         */
         style: PropTypes.oneOfType([
             baseStyle,
             PropTypes.exact({
-                // 回调模式
+                /**
+                 * js函数体字符串
+                 */
                 func: PropTypes.string
             })
         ])
     }),
 
-    // 设置x坐标轴相关属性
+    /**
+     * 配置横坐标轴相关参数，具体见在线文档相关说明
+     */
     xAxis: axisBasePropTypes,
 
-    // 设置y坐标轴相关属性
+    /**
+     * 配置纵坐标轴相关参数，具体见在线文档相关说明
+     */
     yAxis: axisBasePropTypes,
 
-    // 定义图表容器像素宽度，默认为400
+    /**
+     * 图表容器像素宽度
+     */
     width: PropTypes.number,
 
-    // 定义图表容器像素高度，默认为400
+    /**
+     * 图表容器像素高度
+     */
     height: PropTypes.number,
 
-    // 设置图表是否自适应容器宽高，当设置为true时，width与height参数将失效，默认为true
+    /**
+     * 图表是否自适应所在父容器宽高，当`autoFit=True`时，`width`和`height`参数将失效
+     * 默认值：`true`
+     */
     autoFit: PropTypes.bool,
 
-    // 定义图表四个方向的空白间距值，可以为单个数字譬如16，也可以为四个数字构成的数组，按顺序代表上-右-下-左分别的像素间距
+    /**
+     * 画布内边距，传入单个数值表示四周边距，也可传入格式如`[上边距，右边距，下边距，左边距]`的数组，或传入`'auto'`开启底层自动计算
+     */
     padding: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.arrayOf(PropTypes.number),
         PropTypes.oneOf(['auto'])
     ]),
 
-    // 定义在padding基础上额外的像素填充间距
+    /**
+     * 画布额外内边距，传入单个数值表示四周边距，也可传入格式如`[上边距，右边距，下边距，左边距]`的数组
+     */
     appendPadding: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.arrayOf(PropTypes.number)
     ]),
 
-    // 设置图表渲染方式为'canvas'或'svg'模式，默认为'canvas'
+    /**
+     * 图表底层渲染方式，可选项有`'canvas'`和`'svg'`
+     * 默认值：`'canvas'`
+     */
     renderer: PropTypes.oneOf(['canvas', 'svg']),
 
     /**
-     * canvas模式下，控制渲染图表图片的像素比
-     * 默认：1
+     * `renderer='canvas'`时，控制渲染图表图片的像素比
+     * 默认值：`1`
      */
     pixelRatio: PropTypes.number,
 
-    // 设置语言，可选的有'zh-CN'与'en-US'
+    /**
+     * 图表文案语种，可选项有`'zh-CN'`、`'en-US'`
+     * 默认值：`'zh-CN'`
+     */
     locale: PropTypes.oneOf(['zh-CN', 'en-US']),
 
-    // 设置是否对超出绘图区域的几何元素进行裁剪
+    /**
+     * 是否对超出绘图区域的几何元素进行裁剪
+     */
     limitInPlot: PropTypes.bool,
 
-    // 配置图例相关参数
+    /**
+     * 配置图例相关参数，具体见在线文档相关说明
+     */
     legend: legendBasePropTypes,
 
-    // 配置文字标签相关参数
+    /**
+     * 配置数值标签相关参数，具体见在线文档相关说明
+     */
     label: labelBasePropTypes,
 
-    // 设置tooltip相关参数
+    /**
+     * 配置信息框相关参数，具体见在线文档相关说明
+     */
     tooltip: tooltipBasePropTypes,
 
-    // 配置标注相关参数
+    /**
+     * 配置标注相关参数，具体见在线文档相关说明
+     */
     annotations: annotationsBasePropTypes,
 
-    // 配置缩略轴相关参数
+    /**
+     * 配置缩略轴相关参数，具体见在线文档相关说明
+     */
     slider: sliderBasePropTypes,
 
-    // 配置动画相关参数
+    /**
+     * 配置动画相关参数，具体见在线文档相关说明
+     */
     animation: animationBasePropTypes,
 
-    // 常用事件监听参数
-    // tooltip显示事件
+    /**
+     * 事件监听属性，用于监听最近一次信息框显示事件
+     */
     recentlyTooltipChangeRecord: PropTypes.exact({
-        // 事件触发的时间戳信息
+        /**
+         * 事件时间戳
+         */
         timestamp: PropTypes.number,
-
-        // 对应的数据点信息
+        /**
+         * 涉及数据信息
+         */
         data: PropTypes.arrayOf(PropTypes.object)
     }),
 
-    // 单独折线点击事件
+    /**
+     * 事件监听属性，用于监听最近一次折点点击事件
+     */
     recentlyPointClickRecord: PropTypes.exact({
-        // 事件触发的时间戳信息
+        /**
+         * 事件时间戳
+         */
         timestamp: PropTypes.number,
-
-        // 对应的数据点信息
+        /**
+         * 涉及数据信息
+         */
         data: PropTypes.object
     }),
 
-    // 监听图例事件
+    /**
+     * 事件监听属性，用于监听最近一次图例点击事件
+     */
     recentlyLegendInfo: PropTypes.exact({
-        // 记录当前点击的图例项内容
+        /**
+         * 被点击图例项名称
+         */
         triggerItemName: PropTypes.any,
-        // 记录当前各图例项信息
+        /**
+         * 当前各图例项信息
+         */
         items: PropTypes.arrayOf(
             PropTypes.object
         )
     }),
 
-    // 用于在回调中传入uuid、ulid之类的唯一标识，来主动下载当前图表为png格式图片
+    /**
+     * 对当前组件的`downloadTrigger`值进行更新，可实现主动下载当前图表为`png`格式图片
+     */
     downloadTrigger: PropTypes.string,
 
-    // 主题配置
+    /**
+     * 配置主题相关参数，具体见在线文档相关说明
+     */
     theme: themeBasePropTypes,
 
     /**
-     * 交互功能项配置
+     * 配置交互功能相关参数，具体见在线文档相关说明
      */
     interactions: interactionsBasePropTypes,
 
     /**
-     * 状态样式配置
+     * 配置状态样式相关参数，具体见在线文档相关说明
      */
     state: stateBasePropTypes,
 
@@ -269,7 +357,6 @@ AntdLine.propTypes = {
     setProps: PropTypes.func
 };
 
-// 设置默认参数
 AntdLine.defaultProps = {
     locale: 'zh-CN',
     downloadTrigger: 'download-trigger'
